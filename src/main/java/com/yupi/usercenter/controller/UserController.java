5beed49949277244869870b6807041ab93af2fdb
“stefenMiao” <3541138255@qq.com>
2024-08-02 08:30:51 +0800

    注册登录

---
+++ src/main/java/com/yupi/usercenter/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.yupi.usercenter.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.yupi.usercenter.model.domain.User;
+import com.yupi.usercenter.model.domain.request.UserLoginRequest;
+import com.yupi.usercenter.model.domain.request.UserRegisterRequest;
+import com.yupi.usercenter.service.UserService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Resource
+    private UserService userService;
+
+    /**
+     * 用户注册接口
+     * @param userRegisterRequest 用户请求类
+     * @return
+     */
+    @PostMapping("/register")
+    public Long userRegister(@RequestBody UserRegisterRequest userRegisterRequest) {
+        if (userRegisterRequest == null) {
+            return null;
+        }
+        String userAccount = userRegisterRequest.getUserAccount();
+        String userPassword = userRegisterRequest.getUserPassword();
+        String checkPassword = userRegisterRequest.getCheckPassword();
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
+            return null;
+        }
+        Long id= userService.userRegister(userAccount, userPassword, checkPassword);
+        return id;
+    }
+
+
+    @PostMapping("/login")
+    public User userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request) {
+        if (userLoginRequest == null) {
+            return null;
+        }
+        String userAccount = userLoginRequest.getUserAccount();
+        String userPassword = userLoginRequest.getUserPassword();
+        if (StringUtils.isAnyBlank(userAccount, userPassword)) {
+            return null;
+        }
+        User user= userService.userLogin(userAccount, userPassword, request);
+        return user;
+    }
+
+    @GetMapping("/search")
+    public List<User> searchUsers(String username) {
+        QueryWrapper<User> queryWrapper=new QueryWrapper<>();
+
+        if(StringUtils.isNotBlank(username)){
+            queryWrapper.like("username",username);
+        }
+        return userService.list(queryWrapper);
+    }
+
+}
